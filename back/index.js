2e3c4332adef5ada1f9b76babee8e645dba1ce76
MAROUAN17 <marinosaglagal25@gmail.com>
2025-11-10 16:53:51 +0100

    add H3 to retreive user's ip address

--- back/index.js
+++ back/index.js
@@ -1,10 +1,27 @@
 import express from "express";
-import {H3, getRequestIP} from "h3";
+import { getRequestIP } from "h3";
 
 const app = express();
 
+function createH3Event(req) {
+  return {
+    req,
+    node: {
+      req,
+    },
+  };
+}
+
+app.get("/ip", (req, res) => {
+  const h3Event = createH3Event(req);
+  const clientIp = getRequestIP(h3Event);
+  res.status(200).send({ ip: clientIp });
+});
+
 app.get("/", (req, res) => {
-  res.status(200).send({ mssg: req.headers["x-forwarded-for"] || req.socket.remoteAddress });
+  res
+    .status(200)
+    .send({ mssg: req.headers["x-forwarded-for"] || req.socket.remoteAddress });
 });
 
 app.listen(3000, () => {
