f6dcc2d1ca45a97d1c0e3e946d64f20d31bdc47c
MAROUAN17 <marinosaglagal25@gmail.com>
2025-11-12 10:19:42 +0100

    calculating risk score

--- back/index.js
+++ back/index.js
@@ -1,7 +1,19 @@
 import express from "express";
 import { getRequestIP } from "h3";
+import cors from "cors";
+import fs from "fs";
 
 const app = express();
+
+app.use(express.json());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST"],
+    credentials: true,
+  })
+);
 
 function createH3Event(req) {
   return {
@@ -12,18 +24,67 @@ function createH3Event(req) {
   };
 }
 
-app.get("/ip", (req, res) => {
-  const h3Event = createH3Event(req);
-  const clientIp = getRequestIP(h3Event);
-  res.status(200).send({ ip: clientIp });
+function getNested(data, path) {
+  return path.split(".").reduce((acc, curr) => acc && acc[curr], data);
+}
+
+function calculateRiskScore(oldData, newData) {
+  const fields = [
+    ["components.hardware.deviceMemory", 0.2],
+    ["components.hardware.videocard.renderer", 0.2],
+    ["components.system.platform", 0.15],
+    ["components.system.browser.name", 0.1],
+    ["components.system.browser.version", 0.1],
+    ["components.locales.timezone", 0.1],
+    ["components.screen.colorDepth", 0.05],
+    ["components.screen.maxTouchPoints", 0.05], 
+    ["ipAddress", 0.05],
+  ];
+
+  let score = 0;
+
+  for (const [path, weight] of fields) {
+    const oldValue = getNested(newData, path);
+    const newValue = getNested(oldData, path);
+
+    if (oldValue && newValue && oldValue != newValue) {
+      score += weight * 100;
+    }
+  }
+
+  return score;
+}
+
+app.post("/save", (req, res) => {
+  try {
+    let readData = {};
+    let score = 0;
+    const data = req.body.data;
+
+    if (fs.existsSync("data.json")) {
+      const file = fs.readFileSync("data.json");
+      readData = JSON.parse(file);
+    }
+
+    const h3Event = createH3Event(req);
+    const clientIp = getRequestIP(h3Event);
+
+    data.ipAddress = clientIp;
+
+    if (readData)
+      score = calculateRiskScore(readData, data);
+
+    const jsonString = JSON.stringify(data, null, 2);
+    fs.writeFileSync("data.json", jsonString);
+
+    res.status(200).send({ score });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({ mssg: "An Error Occurred" });
+  }
 });
 
-app.get("/", (req, res) => {
-  res
-    .status(200)
-    .send({ mssg: req.headers["x-forwarded-for"] || req.socket.remoteAddress });
-});
-
+//playwright
 app.listen(3000, () => {
   console.log("SERVER STARTED");
 });
--- front/src/components/thumbMark.jsx
+++ front/src/components/thumbMark.jsx
@@ -1,23 +1,26 @@
 import { useEffect, useState } from "react";
 import { Thumbmark } from "@thumbmarkjs/thumbmarkjs";
+import axios from "axios";
 
 export default function ThumbMark() {
   const [info, setInfo] = useState();
+  const [score, setScore] = useState();
   useEffect(() => {
     const tm = new Thumbmark({ apiKey: "1c1f8a186ac04c61a319e41477c602fc" });
-
     tm.get().then((res) => {
-      console.log(res);
       setInfo(res);
+      axios
+        .post("http://localhost:3000/save", { data: res })
+        .then(function (res) {
+          setScore(res.data.score);
+        });
     });
   }, []);
 
   return (
     <>
       <h1>Id: {info?.thumbmark}</h1>
-      <h1>Language: {info?.components.locales.languages}</h1>
-      <h1>userAgent: {info?.components.system.useragent}</h1>
-
+      <h1>Trust: {score}%</h1>
     </>
   );
 }
